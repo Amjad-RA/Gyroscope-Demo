12cbd041c5929875799073da0fbd679aaf1eda2f
AmjadRA <mjdrehman@gmail.com>
2025-02-20 13:11:14 +0530

    Init

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ gyroscope.addEventListener("reading", (e) => {
   console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
   console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
   console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
+  document.getElementById("root").innerHTML = `Angular velocity along the X-axis ${gyroscope.x} Angular velocity along the Y-axis ${gyroscope.y} Angular velocity along the Z-axis ${gyroscope.z}`
 });
 gyroscope.start();
